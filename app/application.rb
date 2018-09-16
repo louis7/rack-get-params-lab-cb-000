b94921ad0f150d0a0280a1e5787a28e2e5281150
louis baptiste <louisvis@ymail.com>
2018-09-16 02:03:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,8 +17,9 @@ class Application
       resp.write handle_search(search_term)
 
     elsif req.path.match(/cart/)
-      if @@cart.length == 0
-        resp.write "cart is empty"
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+    
       else
       @@cart.each do |item|
       resp.write "#{item}\n"
