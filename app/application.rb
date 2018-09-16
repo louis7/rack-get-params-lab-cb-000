a84a01a4c3b38c59b29107581f1d7d85cfcd5a0a
louis baptiste <louisvis@ymail.com>
2018-09-16 01:46:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,16 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      @@cart.each do |item|
+      resp.write "#{item}\n"
+      end
+
     else
       resp.write "Path Not Found"
     end
